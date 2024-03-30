d98ac6c470d4698ceba447ad9b6c347bed6f2d84
yves hoebeke <yves.hoebeke@bytesupply.com>
2024-03-30 14:42:19 -0400

    Correction #2

--- cryptofuncs.go
+++ cryptofuncs.go
@@ -13,10 +13,6 @@ var AlgorithmRegistry map[string]CryptoFunc
 
 type CryptoFunc func([]byte) []byte
 
-func NOP(hash []byte) []byte {
-	return hash[:]
-}
-
 func MD5(hash []byte) []byte {
 	sumResult := md5.Sum(hash)
 	return sumResult[:]
@@ -45,7 +41,7 @@ func SHA512SUM512(hash []byte) []byte {
 // Create function registry
 func init() {
 	AlgorithmRegistry = map[string]CryptoFunc{
-		"NOP":          NOP,
+		"NOP":          MD5,
 		"MD5":          MD5,
 		"SHA1":         SHA1,
 		"SHA256SUM256": SHA256SUM256,
